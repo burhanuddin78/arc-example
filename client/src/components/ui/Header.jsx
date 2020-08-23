3a8b3b22ff9f9853721ce954e6aeb333976fca1e
burhanuddin78 <burhanuddin5023@gmail.com>
2020-08-23 17:12:51 +0500

    make it better

--- client/src/components/ui/Header.jsx
+++ client/src/components/ui/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/ToolBar';
+import { Toolbar } from '@material-ui/core';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/core/styles';
 import logo from '../../assets/logo.svg';
